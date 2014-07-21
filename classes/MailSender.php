acf09ffa82a4492d200e799ee0d023bb01d827ec
Jonathan DO CARMO <jonathandocarmo@jokas.fr>
2014-07-21 15:23:58 +0200

    Proposition fieldsets added

--- classes/MailSender.php
+++ classes/MailSender.php
@@ -72,44 +72,66 @@ class MailSender
         
         if (!self::isMobile()) {
             return '<div id="ms-container">
-                        <div id="ms-client" class="ms-link">
-                            <a href="mailto:' . $this->to . '?bcc=' . $this->bcc . '&cc=' . $this->cc . '&subject=' . $this->subject . '&body=' . $this->body . '" class="ms-bg-img ms-client-bg" title="Client"></a>
-                            <i class="ms-title">Client de messagerie</i>
+                        <p>Acc&eacute;dez directement &agrave; votre messagerie :</p>
+                        <div id="ms-links-wrapper">
+                            <div id="ms-client" class="ms-link">
+                                <a href="mailto:' . $this->to . '?bcc=' . $this->bcc . '&cc=' . $this->cc . '&subject=' . $this->subject . '&body=' . $this->body . '" class="ms-bg-img ms-client-bg" title="Client"></a>
+                                <i class="ms-title">Client de messagerie</i>
+                            </div>
+                            <div id="ms-gmail" class="ms-link">
+                                <div onclick="window.open(\'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-gmail-bg" title="Gmail"></div>
+                                <i class="ms-title">Gmail</i>
+                            </div>
+                            <div id="ms-orange" class="ms-link">
+                                <div onclick="window.open(\'http://webmail.orange.fr/webmail/fr_FR/write.html\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-orange-bg" title="Orange"></div>
+                                <i class="ms-title">Orange</i>
+                            </div>
+                            <div id="ms-sfr" class="ms-link">
+                                <div onclick="window.open(\'https://www.sfr.fr/\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-sfr-bg" title="SFR"></div>
+                                <i class="ms-title">SFR</i>
+                            </div>
+                            <div id="ms-hotmail" class="ms-link">
+                                <div onclick="window.open(\'http://mail.live.com/mail/EditMessageLight.aspx?n=&page=Compose&to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-hotmail-bg" title="Windows Live Hotmail"></div>
+                                <i class="ms-title">Hotmail</i>
+                            </div>
+                            <div id="ms-yahoo" class="ms-link">
+                                <div onclick="window.open(\'http://compose.mail.yahoo.com/?to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-yahoo-bg" title="Yahoo Mail"></div>
+                                <i class="ms-title">Yahoo</i>
+                            </div>
+                            <div id="ms-free" class="ms-link">
+                                <div onclick="window.open(\'http://imp.free.fr/horde/imp/compose.php?to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&subject=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-free-bg" title="Free"></div>
+                                <i class="ms-title">Free</i>
+                            </div>
+                            <div id="ms-laposte" class="ms-link">
+                                <div onclick="window.open(\'https://webmailz.laposte.net/mail#1\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-laposte-bg" title="Laposte"></div>
+                                <i class="ms-title">Laposte</i>
+                            </div>
+                            <div id="ms-numericable" class="ms-link">
+                                <div onclick="window.open(\'https://webmail.numericable.fr/ncn/mail/send.php?msg_to=' . $this->to . '&msg_bcc=' . $this->bcc . '&msg_cc=' . $this->cc . '&msg_subject=' . $this->subject . '&msg_text=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-numericable-bg" title="Num&eacute;ricable"></div>
+                                    <i class="ms-title">Num&eacute;ricable</i>
+                            </div>
+                            <!--<div id="ms-others">
+                            </div>-->
                         </div>
-                        <div id="ms-gmail" class="ms-link">
-                            <div onclick="window.open(\'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-gmail-bg" title="Gmail"></div>
-                            <i class="ms-title">Gmail</i>
-                        </div>
-                        <div id="ms-orange" class="ms-link">
-                            <div onclick="window.open(\'http://webmail.orange.fr/webmail/fr_FR/write.html\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-orange-bg" title="Orange"></div>
-                            <i class="ms-title">Orange</i>
-                        </div>
-                        <div id="ms-sfr" class="ms-link">
-                            <div onclick="window.open(\'https://www.sfr.fr/\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-sfr-bg" title="SFR"></div>
-                            <i class="ms-title">SFR</i>
-                        </div>
-                        <div id="ms-hotmail" class="ms-link">
-                            <div onclick="window.open(\'http://mail.live.com/mail/EditMessageLight.aspx?n=&page=Compose&to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-hotmail-bg" title="Windows Live Hotmail"></div>
-                            <i class="ms-title">Hotmail</i>
-                        </div>
-                        <div id="ms-yahoo" class="ms-link">
-                            <div onclick="window.open(\'http://compose.mail.yahoo.com/?to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&su=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-yahoo-bg" title="Yahoo Mail"></div>
-                            <i class="ms-title">Yahoo</i>
-                        </div>
-                        <div id="ms-free" class="ms-link">
-                            <div onclick="window.open(\'http://imp.free.fr/horde/imp/compose.php?to=' . $this->to . '&bcc=' . $this->bcc . '&cc=' . $this->cc . '&subject=' . $this->subject . '&body=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-free-bg" title="Free"></div>
-                            <i class="ms-title">Free</i>
-                        </div>
-                        <div id="ms-laposte" class="ms-link">
-                            <div onclick="window.open(\'https://webmailz.laposte.net/mail#1\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-laposte-bg" title="Laposte"></div>
-                            <i class="ms-title">Laposte</i>
-                        </div>
-                        <div id="ms-numericable" class="ms-link">
-                            <div onclick="window.open(\'https://webmail.numericable.fr/ncn/mail/send.php?msg_to=' . $this->to . '&msg_bcc=' . $this->bcc . '&msg_cc=' . $this->cc . '&msg_subject=' . $this->subject . '&msg_text=' . $this->body . '\', \'\' ,\'width=800, height=600, menubar=yes\')" class="ms-bg-img ms-numericable-bg" title="Num&eacute;ricable"></div>
-                                <i class="ms-title">Num&eacute;ricable</i>
-                        </div>
-                        <!--<div id="ms-others">
-                        </div>-->
+                        <fieldset id="ms-text-proposition">
+                            <legend>Proposition de texte</legend>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis viverra tortor imperdiet malesuada adipiscing. Quisque varius placerat diam, in elementum urna aliquam at. Vivamus iaculis eleifend vestibulum. Quisque ac erat leo. Aliquam ultrices et lorem eget convallis. Duis sed ante sed est sollicitudin tempor nec auctor erat. Proin eget risus magna. Aenean vulputate, nibh nec auctor pulvinar, justo quam posuere neque, et fringilla eros turpis et mauris. Phasellus euismod at magna eget porttitor.</p>
+
+                            <p>Mauris ornare lacinia condimentum. Pellentesque interdum elit a dolor vehicula, ac condimentum arcu adipiscing. Mauris in pulvinar libero. Maecenas mauris metus, dignissim eu erat eu, volutpat dictum metus. Integer imperdiet turpis at arcu interdum, scelerisque elementum nulla ultrices. Maecenas semper metus at ligula bibendum, a tempus velit faucibus. Suspendisse mauris ante, tristique sit amet pellentesque nec, vestibulum quis sapien.</p>
+
+                            <p>Proin rhoncus, dolor non facilisis viverra, ligula sapien suscipit purus, et tempor erat justo eget lectus.</p>
+                            <!--<div class="centerize">
+                                <button class="ms-btn-copy">Copier</button>
+                            </div>-->
+                        </fieldset>
+                        <fieldset id="ms-recipients-proposition">
+                            <legend>Destinataires</legend>
+                            <p>example01@gmail.com</p>
+                            <p>example02@hotmail.com</p>
+                            <!--<div class="centerize">
+                                <button class="ms-btn-copy">Copier</button>
+                            </div>-->
+                        </fieldset>
                     </div>';
         } else {
             return '<div id="ms-container">
